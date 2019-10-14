cd3e4b8ebf982aba2fcdfa4b5cce9102635fd1a0
Katrina Roaix <kroaix@gmail.com>
2019-10-13 22:41:19 -0400

    Wrapped App in container

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,25 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Container } from "semantic-ui-react";
 
 import Login from "./components/Login";
 import "./styles.scss";
+import "semantic-ui-css/semantic.min.css";
 
 function App() {
   const [colorList, setColorList] = useState([]);
   return (
-    <Router>
-      <div className="App">
-        <Route exact path="/" component={Login} />
-        {/* 
-          Build a PrivateRoute component that will 
-          display BubblePage when you're authenticated 
-        */}
-      </div>
-    </Router>
+    <Container>
+      <Router>
+        <div className="App">
+          <Route exact path="/" component={Login} />
+          {/* 
+          Build a PrivateRoute component that will 
+          display BubblePage when you're authenticated 
+        */}
+        </div>
+      </Router>
+    </Container>
   );
 }
 
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,13 +1,19 @@
 import React from "react";
+import { Form, Input, Button } from "semantic-ui-react";
 
 const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
+  // make a post request to retrieve a token from the api
+  // when you have handled the token, navigate to the BubblePage route
   return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
+    <div>
+      <h1>Welcome to the Bubble App!</h1>
+      <p>Build a login page here</p>
+      <Form>
+        <Input />
+        <Input />
+          <Button>Log In</Button>
+      </Form>
+    </div>
   );
 };
 
