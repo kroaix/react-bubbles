00d53c32a110845c346b43d47870c4660fe19754
Katrina Roaix <kroaix@gmail.com>
2019-10-13 22:57:18 -0400

    Set up routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,20 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import { Container } from "semantic-ui-react";
 
-import Login from "./components/Login";
+import Routes from './utils/Routes';
+
 import "./styles.scss";
 import "semantic-ui-css/semantic.min.css";
 
 function App() {
   const [colorList, setColorList] = useState([]);
+
   return (
     <Container>
       <Router>
         <div className="App">
-          <Route exact path="/" component={Login} />
-          {/* 
-          Build a PrivateRoute component that will 
-          display BubblePage when you're authenticated 
-        */}
+          <Routes />
         </div>
       </Router>
     </Container>
